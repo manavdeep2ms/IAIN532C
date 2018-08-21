f268ba9410d24d834411acd5cc75117cc571d9fb
manavdeep2ms <iit2016042@iiita.ac.in>
2018-08-21 15:58:31 +0530

    lab assignment 1

---
+++ lab1.pl
@@ -0,0 +1,248 @@
+male(manav).
+male(anmol).
+male(charan).
+male(rollicks).
+male(uppi).
+male(akki).
+male(arrow).
+male(nandan).
+male(prasoon).
+male(malu).
+male(arnav).
+male(prakash).
+male(ballu).
+male(ayush).
+male(hj).
+
+female(pallak).
+female(mansi).
+female(shubhangi).
+female(muskan).
+female(rashan).
+female(lovely).
+female(sindhu).
+female(jasgun).
+female(jhanvi).
+female(sangeeta).
+female(snigdha).
+female(komal).
+female(jasmeet).
+female(lithika).
+female(sakshi).
+
+lives(manav, amritsar).
+lives(rashan, bathinda).
+lives(anmol, amritsar).
+lives(charan, andhra-pradesh).
+lives(rollicks, vizag).
+lives(uppi, guwahati).
+lives(akki, nagrota).
+lives(arrow, jaipur).
+lives(nandan, patna).
+lives(prasoon, bathinda).
+lives(malu, indore).
+lives(arnav, bathinda).
+lives(prakash, nepal).
+lives(ballu, ranchi).
+lives(ayush, pilani).
+lives(hj, bathinda).
+lives(pallak, new-delhi).
+lives(mansi, alwar).
+lives(shubhangi, unnao).
+lives(muskan, hisar).
+lives(lovely, pathankot).
+lives(sindhu, andhra-pradesh).
+lives(snigdha, dehradun).
+lives(rashan, bathinda).
+lives(jasgun, amritsar).
+lives(jhanvi, gwalior).
+lives(sangeeta, amritsar).
+lives(lithika, hubli).
+lives(sakshi, jhansi).
+lives(komal, dhanbad).
+
+specialist(stomach-flu, manav).
+specialist(asthma, rashan).
+specialist(kidney-stone, anmol).
+specialist(jaundice, rollicks).
+specialist(malaria, charan).
+specialist(dengue, sindhu).
+specialist(adhd, uppi).
+specialist(ligament-tear, akki).
+specialist(als, arrow).
+specialist(alzheimers, nandan).
+specialist(fracture, prasoon).
+specialist(meningitis, malu).
+specialist(bronchitis, arnav).
+specialist(cancer, prakash).
+specialist(arthiritis, ballu).
+specialist(cerebral-palsy, ayush).
+specialist(chicken-pox, hj).
+specialist(cholera, mansi).
+specialist(fever, pallak).
+specialist(common-cold, shubhangi).
+specialist(cramps, muskan).
+specialist(diabetes, sangeeta).
+specialist(downs-syndrome, lithika).
+specialist(e-coli, snigdha).
+specialist(ebola, komal).
+specialist(nipah, lovely).
+specialist(epilepsy, jhanvi).
+specialist(hypothermia, jasgun).
+specialist(food-poisoning, jasmeet).
+specialist(acidity, sakshi).
+
+symptomOf(shortness-of-breath, asthma).
+symptomOf(cough, asthma).
+symptomOf(wheezing, asthma).
+symptomOf(chest-tightness, asthma).
+symptomOf(chest-pain, asthma).
+symptomOf(chest-pressure, asthma).
+symptomOf(stomach-ache, stomach-flu).
+symptomOf(stomach-inflammation, stomach-flu).
+symptomOf(diarrhea, stomach-flu).
+symptomOf(vomiting, stomach-flu).
+symptomOf(abdominal-cramps, stomach-flu).
+symptomOf(cloudy-urine, kidney-stone).
+symptomOf(back-pain, kidney-stone).
+symptomOf(belly-pain, kidney-stone).
+symptomOf(pain-burning-during-urination, kidney-stone).
+symptomOf(urgency, kidney-stone).
+symptomOf(bloody-urine, kidney-stone).
+symptomOf(fatigue, jaundice).
+symptomOf(abdominal-pain, jaundice).
+symptomOf(weight-loss, jaundice).
+symptomOf(vomiting, jaundice).
+symptomOf(fever, jaundice).
+symptomOf(pale-stools, jaundice).
+symptomOf(dark-urine, jaundice).
+symptomOf(profuse-sweating, malaria).
+symptomOf(headache, malaria).
+symptomOf(nausea, malaria).
+symptomOf(vomiting, malaria).
+symptomOf(abdominal-pain, malaria).
+symptomOf(diarrhea, malaria).
+symptomOf(fatigue, dengue).
+symptomOf(nausea, dengue).
+symptomOf(vomiting, dengue).
+symptomOf(skin-rash, dengue).
+symptomOf(severe-joint-muscle-pain, dengue).
+symptomOf(agression, adhd).
+symptomOf(excitablity, adhd).
+symptomOf(fidgeting, adhd).
+symptomOf(hyperactivity, adhd).
+symptomOf(impulsivity, adhd).
+symptomOf(irritability, adhd).
+symptomOf(lack-of-restraint, adhd).
+symptomOf(absent-mindedness, adhd).
+symptomOf(difficult-focusing, adhd).
+symptomOf(forgetfulness, adhd).
+symptomOf(bruising, ligament-tear).
+symptomOf(pain-when-standing, ligament-tear).
+symptomOf(limping, ligament-tear).
+symptomOf(swelling, ligament-tear).
+symptomOf(muscle-weakness, als).
+symptomOf(cramping, als).
+symptomOf(problems-with-coordination, als).
+symptomOf(stiff-muscles, als).
+symptomOf(loss-of-muscle, als).
+symptomOf(muscle-spasms, als).
+symptomOf(overactive-reflexes, als).
+symptomOf(mental-decline, alzheimers).
+symptomOf(difficulty-thinking-understanding, alzheimers).
+symptomOf(confusion-in-the-evening-hours, alzheimers).
+symptomOf(delusion, alzheimers).
+symptomOf(disorientation, alzheimers).
+symptomOf(forgetfulness, alzheimers).
+symptomOf(making-things-up, alzheimers).
+symptomOf(mental-confusion, alzheimers).
+symptomOf(difficulty-concentrating, alzheimers).
+symptomOf(inability-to-create-new-memories, alzheimers).
+symptomOf(inability-todo-simple-maths, alzheimers).
+symptomOf(inability-to-recognise-common-things, alzheimers).
+symptomOf(fever, meningitis).
+symptomOf(chills, meningitis).
+symptomOf(lethargy, meningitis).
+symptomOf(loss-of-appetite, meningitis).
+symptomOf(bleeding, fracture).
+symptomOf(bruising, fracture).
+symptomOf(limping, fracture).
+symptomOf(abnormal-bumps, cancer).
+symptomOf(unexplained-fevers, cancer).
+symptomOf(night-sweats, cancer).
+symptomOf(unintentional-weight-loss, cancer).
+symptomOf(pain, arthiritis).
+symptomOf(swelling, arthiritis).
+symptomOf(reduced-range-of-motion, arthiritis).
+symptomOf(stiffness, arthiritis).
+symptomOf(exaggerated-reflexes, cerebral-palsy).
+symptomOf(floppy-rigid-limbs, cerebral-palsy).
+symptomOf(involuntary-motions, cerebral-palsy).
+symptomOf(itchy-blister-like-rash-on-the-skin, chicken-pox).
+symptomOf(diarrhoea, cholera).
+symptomOf(dehydration, cholera).
+symptomOf(sweating, fever).
+symptomOf(chills-shivering, fever).
+symptomOf(headache, fever).
+symptomOf(runny-nose, common-cold).
+symptomOf(sneezing, common-cold).
+symptomOf(congestion, common-cold).
+symptomOf(swelling, cramps).
+symptomOf(redness, cramps).
+symptomOf(skin-changes, cramps).
+symptomOf(excessive-thirst, diabetes).
+symptomOf(excessive-urination, diabetes).
+symptomOf(fatigue, diabetes).
+symptomOf(weight-loss, diabetes).
+symptomOf(blurred-vision, diabetes).
+symptomOf(distinct-facial-appearance, downs-syndrome).
+symptomOf(intellectual-disability, downs-syndrome).
+symptomOf(developmental-delays, downs-syndrome).
+symptomOf(nausea, e-coli).
+symptomOf(vomiting, e-coli).
+symptomOf(abdominal-pain, e-coli).
+symptomOf(bloody-diarrhea, e-coli).
+symptomOf(fever, ebola).
+symptomOf(headache, ebola).
+symptomOf(muscle-pain, ebola).
+symptomOf(coughing-blood, ebola).
+symptomOf(fever, nipah).
+symptomOf(headache, nipah).
+symptomOf(myalgia, nipah).
+symptomOf(sore-throat, nipah).
+symptomOf(vomiting, nipah).
+symptomOf(seizures, epilepsy).
+symptomOf(amnesia, epilepsy).
+symptomOf(anxiety, epilepsy).
+symptomOf(depression, epilepsy).
+symptomOf(staring-spells, epilepsy).
+
+
+
+remedyOf('ginger, mint or lemon water', stomach-flu).
+remedyOf(quinone, malaria).
+remedyOf(inhalers, asthma).
+remedyOf('drinking more water', kidney-stone).
+remedyOf(sunlight, jaundice).
+remedyOf(fluids, dengue).
+remedyOf(talk-therapy, adhd).
+remedyOf(ice, ligament-tear).
+remedyOf(nil, als).
+remedyOf('cognition enhancing mechanism', alzheimers).
+remedyOf(rest, fracture).
+remedyOf(penicillin, meningitis).
+remedyOf(analgesic, bronchitis).
+remedyOf(chemo-therapy, cancer).
+remedyOf(yoga, arthiritis).
+remedyOf(muscle-relaxant, cerebral-palsy).
+remedyOf(rest, chicken-pox).
+remedyOf(antibiotic, cholera).
+remedyOf(paracetamol, fever).
+remedyOf(antibiotic, common-cold).
+remedyOf(volini, cramps).
+remedyOf('lots of water', e-coli).
+remedyOf('blood transfusion', ebola).
+remedyOf(nil, nipah).
+remedyOf(aloe-vera, diabetes).
+remedyOf('support group', downs-syndrome).
+remedyOf(garlic, epilepsy).
